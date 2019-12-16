805ea0d60dc28d9afdab09028f3004a4214b3fcf
ColinV03 <51303969+ColinV03@users.noreply.github.com>
2019-12-16 17:41:14 -0500

    Add files via upload
    Initial push of game.

---
+++ script.js
@@ -0,0 +1,101 @@
+// 
+// 
+// let userChoice = prompt("Rock, Paper, or Scissors");
+// 
+// let computerChoice = Math.random();
+// 
+// if(computerChoice<=0.33){
+//     computerChoice = "rock";
+// } else if (0.34<=computerChoice <= 0.66){
+//     computerChoice = "paper";
+// } else {
+//     computerChoice = "scissors";
+// }
+// 
+// let computerScore = 0;
+// let userScore = 0;
+// 
+// 
+// choice1 = userChoice
+// choice2 = computerChoice
+// 
+// allows computer and user to select, variables stored as strings **//
+let userScore = 0;
+let computerScore = 0;
+
+
+
+function game() {
+    
+    for( i = 0; i <= 5; i++) {
+        selections();
+        playRound(); 
+        scores();
+    }
+}
+
+function playRound(choice1, choice2){
+
+    if(choice1 === choice2){
+        return("The result is a tie");
+    }
+    if(choice1 === "rock"){
+        if(choice2 === "scissors"){
+            userScore++;
+            return ("Rock Wins")
+        } else {
+            computerScore++;
+            return("paper Wins!")
+        }
+    }
+
+    if(choice1 === "paper"){
+        if(choice2 === "rock"){
+            userScore++;
+            return ("paper Wins")
+        } else {
+            computerScore++;
+            return("scissors Wins!")
+        }
+    }
+
+    if(choice1 === "scissors"){
+        if(choice2 === "paper"){
+            userScore++;
+            return ("Scissors Wins")
+        } else {
+            computerScore++;
+            return("rock Wins!")
+        }
+    }
+    
+}
+
+
+function selections() {
+    let userChoice = prompt("Rock, Paper, or Scissors");
+
+    let computerChoice = Math.random();
+
+    if(computerChoice<=0.33){
+      computerChoice = "rock";
+    } else if (0.34<=computerChoice <= 0.66){
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    choice1 = userChoice
+    choice2 = computerChoice
+
+}
+
+function scores() {
+    console.log("You picked " + choice1);
+    console.log("The computer picked " + choice2);
+    console.log(playRound(choice1,choice2));
+    console.log(userScore + " is your Score");
+    console.log(computerScore + " is the computer's score");
+}
+
+game();
